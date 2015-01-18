5ad82ac80ab5b9df3b9a85a3ff824df6d706a261
Systemic <trash@laverriere.org>
2015-01-18 17:51:51 +0000

    Hard reset button addition

---
+++ config/RPi_h_r.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25,GPIO.IN)
+
+button_previous = 1
+button_current = 1
+brojac = 0
+flag_pressed = 0
+
+while True:
+
+  button_current = GPIO.input(25);
+  flag_pressed = button_previous + button_current
+
+  if (not(flag_pressed)):
+    brojac += 1
+  else:
+    brojac = 0
+
+  if (button_current and (not button_previous)):
+    #print("Reset")  
+    os.system("sudo shutdown -r now")
+  if ((not flag_pressed) and  brojac >= 100):
+    #print("Shutdown")
+    os.system("sudo shutdown -h now")
+    break
+  
+  button_previous = button_current
+  time.sleep(0.03)
+
--- config/setup.sh
+++ config/setup.sh
@@ -1,18 +1,26 @@
 sudo cp cmdline.txt /boot/cmdline.txt
 sudo cp raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
 sudo cp modules /etc/modules
+sudo cp RPi_h_r.py /home/pi/RPi_h_r.py
+sudo cp rc.local /etc/rc.local
 
 sudo chown root:root /boot/cmdline.txt
 sudo chown root:root /etc/modprobe.d/raspi-blacklist.conf
 sudo chown root:root /etc/modules
+sudo chown root:root /home/pi/RPi_h_r.py
+sudo chown root:root /etc/rc.local
 
 sudo chmod 755 /boot/cmdline.txt
 sudo chmod 644 /etc/modprobe.d/raspi-blacklist.conf
 sudo chmod 644 /etc/modules
+sudo chmod 755 /hom/pi/RPi_h_r.py
+sudo chmod 755 /boot/cmdline.txt
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install wicd-curses
+sudo apt-get -y install atftpd
+sudo apt-get -y install screen
 
 sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update
 sudo reboot
